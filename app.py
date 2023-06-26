48e65456f0f198bfe49b066acbe64469986a446b
EC2 Default User <ec2-user@ip-172-31-35-158.ec2.internal>
2023-06-26 09:42:38 +0000

    docstring

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """return text"""
     return "Hello, my name is Fred"
 if __name__ == "__main__":
-    """run app"""
     app.run(host="0.0.0.0", port=80)
